d6316194e7f40216c6f437b9c9b0d781c64bbb6e
Calebjaytech <calebtech4real@gmail.com>
2022-12-04 10:56:48 -0800

    done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     from sys import argv
     argv.pop(0)
     argvLen = len(argv)
-  i = 0
+    i = 0
     res = 0
     if argvLen != 0:
         while i < argvLen:
             res = res + int(argv[i])
             i = i + 1
-   print("{:d}".format(res), end="\n")
+        print("{:d}".format(res), end="\n")
     else:
         print("{:d}".format(0), end="\n")
