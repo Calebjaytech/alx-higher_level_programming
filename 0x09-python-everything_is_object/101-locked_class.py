487c60f7b8330a318175234144c03bc0e7fd21f7
Calebjaytech <calebtech4real@gmail.com>
2023-01-16 11:08:58 -0800

    fix

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Locked Clas
+"'"
+
+
+
+
+
+class LockedClass:
+
+    """ Prevent user to create new attributes """
+
+    __slots__ = ['first_name']
+
+
+
+    def __init__(self, first_n=""):
+
+        self.first_name = 
