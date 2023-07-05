46097a7ed19aebf8418eb63bcf1bf7210825a24b
Caleb Jacob <110934183+Calebjaytech@users.noreply.github.com>
2023-07-05 21:52:29 +0100

    git commit

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# display the only the content of the body
-curl -sL "$1"
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#  send a post a requst with json file
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
